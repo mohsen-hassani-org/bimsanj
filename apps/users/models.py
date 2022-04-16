f9a553a4b7d8842e893ed9627d5a90530318ecb7
Mohsen Hassani <m.hassani.de@gmail.com>
2022-04-16 18:35:18 +0430

    Fix issue in migrating referral_code

--- apps/users/models.py
+++ apps/users/models.py
@@ -10,8 +10,9 @@ from .managers import CustomUserManager
 
 def generate_referral():
     code = generate_random_string(use_lower=False, use_symbols=False, length=8)
-    while(User.objects.filter(referral_code=code).exists()):
-        code = generate_random_string(use_lower=False, use_symbols=False, length=8)
+    if "referral_code" in [field.name for field in User._meta.get_fields()]:
+        while(User.objects.filter(referral_code=code).exists()):
+            code = generate_random_string(use_lower=False, use_symbols=False, length=8)
     return code
 
 
