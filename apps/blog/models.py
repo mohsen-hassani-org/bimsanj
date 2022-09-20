0e8e6cf891ad077b273a71c0547b30c05f677308
Mohsen Hassani <m.hassani.de@gmail.com>
2022-09-20 10:48:35 +0400

    Add __str__ method to tags model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -25,6 +25,9 @@ class Tag(Timestampable, Permalinkable, models.Model):
     title = models.CharField(max_length=255, verbose_name='عنوان')
     description = models.TextField(verbose_name='توضیحات', blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Category(Timestampable, Permalinkable, Publishable, models.Model):
     title = models.CharField(max_length=255, verbose_name='عنوان')
