faa3e94d6df30fb50b78ecbc67e0c9f45b85c9a3
likecodingloveproblems <mohammadali.mehdizadeh@outlook.com>
2022-09-21 09:46:51 +0430

    cache key may become so complex in future, so I move it into a function and add module name to it

--- apps/core/mixins.py
+++ apps/core/mixins.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
@@ -6,6 +7,7 @@ from django.core.cache import cache
 
 class SingletonMixin:
     """An abstract base class that provides a Singleton pattern for models."""
+
     class Meta:
         abstract = True
 
@@ -16,17 +18,22 @@ class SingletonMixin:
 
     @classmethod
     def load(cls):
-        if cache.get(cls.__name__) is None:
+        if cache.get(cls.cache_key()) is None:
             obj, created = cls.objects.get_or_create(pk=1)
             if not created:
                 obj._set_cache()
-        return cache.get(cls.__name__)
+        return cache.get(cls.cache_key())
 
     def delete(self, *args, **kwargs):
         pass
 
     def _set_cache(self):
-        cache.set(self.__class__.__name__, self)
+        cache.set(self.cache_key(), self)
+
+    @classmethod
+    @lru_cache
+    def cache_key(cls):
+        return f"{cls.__module__}:{cls.__name__}"
 
 
 class Timestampable(models.Model):
@@ -36,7 +43,7 @@ class Timestampable(models.Model):
     class Meta:
         abstract = True
 
-        
+
 class LogicalDeletable(models.Model):
     is_deleted = models.BooleanField(default=False)
 
@@ -45,11 +52,11 @@ class LogicalDeletable(models.Model):
 
     def delete(self, using=None, keep_parents=False):
         self.is_deleted = True
-        self.save(update_fields=['is_deleted'])
+        self.save(update_fields=["is_deleted"])
 
     def restore(self):
         self.is_deleted = False
-        self.save(update_fields=['is_deleted'])
+        self.save(update_fields=["is_deleted"])
 
 
 class Activable(models.Model):
@@ -60,11 +67,11 @@ class Activable(models.Model):
 
     def activate(self):
         self.is_active = True
-        self.save(update_fields=['is_active'])
+        self.save(update_fields=["is_active"])
 
     def deactivate(self):
         self.is_active = False
-        self.save(update_fields=['is_active'])
+        self.save(update_fields=["is_active"])
 
 
 class Permalinkable(models.Model):
@@ -74,10 +81,12 @@ class Permalinkable(models.Model):
         abstract = True
 
     def get_url_kwargs(self, **kwargs):
-        kwargs.update(getattr(self, 'url_kwargs', {}))
+        kwargs.update(getattr(self, "url_kwargs", {}))
         return kwargs
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
         if not self.slug:
             self.slug = slugify(self.slug_source, allow_unicode=True)
         return super().save(force_insert, force_update, using, update_fields)
@@ -92,7 +101,7 @@ class Permalinkable(models.Model):
 
 
 class Publishable(models.Model):
-    publish_date = models.DateTimeField(null=True, verbose_name='تاریخ انتشار')
+    publish_date = models.DateTimeField(null=True, verbose_name="تاریخ انتشار")
 
     class Meta:
         abstract = True
@@ -101,15 +110,17 @@ class Publishable(models.Model):
         if not date:
             date = timezone.now()
         self.publish_date = date
-        self.save(update_fields=['publish_date'])
+        self.save(update_fields=["publish_date"])
 
     @property
     def is_published(self):
         return self.publish_date and self.publish_date < timezone.now()
 
-        
+
 class Authorable(models.Model):
-    created_by = models.ForeignKey('users.User', on_delete=models.PROTECT, verbose_name='ایجاد کننده')
+    created_by = models.ForeignKey(
+        "users.User", on_delete=models.PROTECT, verbose_name="ایجاد کننده"
+    )
 
     class Meta:
         abstract = True
