17474cb5d0cce074d43d4d96b8c5d94c64f7e935
likecodingloveproblems <mohammadali.mehdizadeh@outlook.com>
2022-09-21 09:50:39 +0430

    postgres database connection is setup

--- Bimsanj/settings.py
+++ Bimsanj/settings.py
@@ -20,124 +20,128 @@ def getenv(key, default=None):
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = getenv('SECRET_KEY', 'foobar')
-DEBUG = bool(getenv('DEBUG', default=0))
-ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', default='*').split(',')
+SECRET_KEY = getenv("SECRET_KEY", "foobar")
+DEBUG = bool(getenv("DEBUG", default=0))
+ALLOWED_HOSTS = getenv("ALLOWED_HOSTS", default="*").split(",")
 
 DJANGO_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 THIRD_PARTY_APPS = [
-    'django_extensions',
-    'jalali_date',
-    'widget_tweaks',
-    'crispy_forms',
-    'rest_framework',
-    'django_quill',
+    "django_extensions",
+    "jalali_date",
+    "widget_tweaks",
+    "crispy_forms",
+    "rest_framework",
+    "django_quill",
 ]
 PROJECT_APPS = [
-    'apps.users',
-    'apps.core',
-    'apps.accounts',
-    'apps.blog',
-    'apps.sms',
-    'apps.insurance',
-    'apps.my_theme',
-    'apps.perry',
+    "apps.users",
+    "apps.core",
+    "apps.accounts",
+    "apps.blog",
+    "apps.sms",
+    "apps.insurance",
+    "apps.my_theme",
+    "apps.perry",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'apps.blog.context_processors.site_settings',
-                'apps.blog.context_processors.theme_content',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "apps.blog.context_processors.site_settings",
+                "apps.blog.context_processors.theme_content",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Bimsanj.wsgi.application'
-ROOT_URLCONF = 'Bimsanj.urls'
+WSGI_APPLICATION = "Bimsanj.wsgi.application"
+ROOT_URLCONF = "Bimsanj.urls"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DB_NAME", "bimsanj"),
+        "USER": getenv("DB_USER", "bimsanj"),
+        "PASSWORD": getenv("DB_PASSWORD", "12345"),
+        "HOST": getenv("DB_HOST", "127.0.0.1"),
+        "PORT": getenv("DB_PORT", "5432"),
     }
 }
 
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'fa'
-TIME_ZONE = 'Asia/Tehran'
+LANGUAGE_CODE = "fa"
+TIME_ZONE = "Asia/Tehran"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "media/"
 
 # BLOG_THEME = 'blog'
 # BLOG_THEME = 'my_theme'
-BLOG_THEME = 'perry'
+BLOG_THEME = "perry"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.User'
-LOGIN_URL = reverse_lazy('auth:get_phone')
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.User"
+LOGIN_URL = reverse_lazy("auth:get_phone")
 
 REST_FRAMEWORK = {
     # "DATE_INPUT_FORMATS": ["%d/%m/%Y"],
 }
 
-DEFAULT_BIG_SLIDER_URL = 'https://via.placeholder.com/1920x1080'
-DEFAULT_THUMBNAIL_URL = 'https://via.placeholder.com/300x300'
+DEFAULT_BIG_SLIDER_URL = "https://via.placeholder.com/1920x1080"
+DEFAULT_THUMBNAIL_URL = "https://via.placeholder.com/300x300"
 
 try:
     from Bimsanj.local_settings import *
